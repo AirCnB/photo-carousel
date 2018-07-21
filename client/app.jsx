08cb8433f8f662ca095b64f21201e94043e15dfb
Arthur Hovanesian <nameonascreen2@gmail.com>
2018-07-20 19:19:57 -0700

    Checkout to master

--- client/app.jsx
+++ client/app.jsx
@@ -22,17 +22,20 @@ class App extends React.Component {
     .then(res => this.setState({
       pictures: res.data[0]
     }))
-    .then(res => {console.log(res)})
   }
 
   render() {
     console.log(this.state.pictures)
     return (
-      <div className="container">
+      <div>
+      <div className="container" style={{backgroundImage: `url(${this.state.pictures.photos[0].url})`}}>
         <div className="content">
-          <img src={this.state.pictures.photos[0].url}/>
           <div><br /><br /><br /></div>
         </div>
+        <div>
+          <button>Hello</button>
+        </div>
+      </div>
       </div>
     )
   }
