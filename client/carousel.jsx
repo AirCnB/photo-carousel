7c12b041410b8e085df5c14a1456f3a4cc305796
Arthur Hovanesian <nameonascreen2@gmail.com>
2018-07-24 15:54:41 -0700

    Add back and forward buttons for carousel

--- client/carousel.jsx
+++ client/carousel.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/carousel.css';
-//pictures
+//pictures, toggleCarousel
 class Carousel extends React.Component {
 
   constructor(props) {
@@ -13,10 +13,22 @@ class Carousel extends React.Component {
       <div className='popup'>
         <div className='popup_inner'>
           <div>
-          <svg className="exit" height="25" width="25">
-            <line x1="0" y1="0" x2="25" y2="25" />
-            <line x1="25" y1="0" x2="0" y2="25" />
-          </svg>
+            <svg onClick={() => this.props.toggleCarousel()} className="exit" height="25" width="25">
+              <line x1="0" y1="0" x2="25" y2="25" />
+              <line x1="25" y1="0" x2="0" y2="25" />
+            </svg>
+          </div>
+          <div className="center">
+            <div className="back">
+              <svg className="backbutton" viewBox="0 0 18 18" role="presentation" aria-hidden="true" focusable="false">
+                <path d="m13.7 16.29a1 1 0 1 1 -1.42 1.41l-8-8a1 1 0 0 1 0-1.41l8-8a1 1 0 1 1 1.42 1.41l-7.29 7.29z" fill-rule="evenodd"></path>
+              </svg>
+            </div>
+            <div className="forward">
+              <svg className="forwardbutton" viewBox="0 0 18 18" role="presentation" aria-hidden="true" focusable="false">
+                <path d="m4.29 1.71a1 1 0 1 1 1.42-1.41l8 8a1 1 0 0 1 0 1.41l-8 8a1 1 0 1 1 -1.42-1.41l7.29-7.29z" fill-rule="evenodd"></path>
+              </svg>
+            </div>
           </div>
         </div>
       </div>
