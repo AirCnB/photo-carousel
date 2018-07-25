a1ff0a2a6a3e9c259d684d1d4d3ad6c6bf38bf33
Arthur Hovanesian <nameonascreen2@gmail.com>
2018-07-24 18:06:26 -0700

    Resize displayed photo with window resize

--- client/carousel.jsx
+++ client/carousel.jsx
@@ -19,6 +19,7 @@ class Carousel extends React.Component {
             </svg>
           </div>
           <div className="center">
+            <img className="pic" src={this.props.pictures.photos[0].url}/>
             <div className="back">
               <svg className="backbutton" viewBox="0 0 18 18" role="presentation" aria-hidden="true" focusable="false">
                 <path d="m13.7 16.29a1 1 0 1 1 -1.42 1.41l-8-8a1 1 0 0 1 0-1.41l8-8a1 1 0 1 1 1.42 1.41l-7.29 7.29z" fill-rule="evenodd"></path>
@@ -39,3 +40,7 @@ class Carousel extends React.Component {
 export default Carousel
 
 // <img src={this.props.pictures.photos[0].url} />
+
+// <div className="picbox">
+//   <img className="pic" src={this.props.pictures.photos[0].url}/>
+// </div>
