d055e100e5f191e76797c21702528c34f37cb302
Arthur Hovanesian <nameonascreen2@gmail.com>
2018-07-26 14:16:11 -0700

    Add photo descriptions to slider

--- src/components/photoview.jsx
+++ src/components/photoview.jsx
@@ -8,21 +8,25 @@ class PhotoView extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      mainPhoto: this.props.pictures.photos[0].url
+      pictures: {},
+      mainPhoto: this.props.pictures.photos[0]
     }
   }
 
   componentDidMount() {
-    for (let i = 0; i < this.props.pictures.photos.length; i++) {
-      this.props.pictures.photos[i].index = i;
-    }
     document.addEventListener("keydown", this.handleKeyDown.bind(this));
   }
 
   selectPhoto(event) {
-    const src = event.getAttribute('src');
+    let selected;
+    for (let i = 0; i < this.props.pictures.photos.length; i++) {
+      if (event.getAttribute('src') === this.props.pictures.photos[i].url) {
+        selected = this.props.pictures.photos[i]
+      }
+    }
+    console.log(selected)
     this.setState({
-      mainPhoto: src
+      mainPhoto: selected
     })
   }
 
@@ -30,9 +34,9 @@ class PhotoView extends React.Component {
     const photos = this.props.pictures.photos;
     const numPhotos = this.props.pictures.photos.length;
     for (let i = 0; i < numPhotos; i++) {
-      if (photos[i].url === this.state.mainPhoto) {
+      if (photos[i].url === this.state.mainPhoto.url) {
         this.setState({
-          mainPhoto: photos[(i + 1) % numPhotos].url
+          mainPhoto: photos[(i + 1) % numPhotos]
         })
         break;
       }
@@ -43,9 +47,9 @@ class PhotoView extends React.Component {
     const photos = this.props.pictures.photos;
     const numPhotos = this.props.pictures.photos.length;
     for (let i = 0; i < numPhotos; i++) {
-      if (photos[i].url === this.state.mainPhoto) {
+      if (photos[i].url === this.state.mainPhoto.url) {
         this.setState({
-          mainPhoto: photos[(((i - 1) % numPhotos) + numPhotos) % numPhotos].url
+          mainPhoto: photos[(((i - 1) % numPhotos) + numPhotos) % numPhotos]
         })
         break;
       }
@@ -72,7 +76,7 @@ class PhotoView extends React.Component {
             </svg>
           </div>
           <div className="center">
-            <img onClick={this.selectNext.bind(this)} className="pic" src={this.state.mainPhoto}/>
+            <img onClick={this.selectNext.bind(this)} className="pic" src={this.state.mainPhoto.url}/>
             <div className="back" onClick={this.selectPrevious.bind(this)}>
               <svg className="backbutton" viewBox="0 0 18 18" role="presentation" aria-hidden="true" focusable="false">
                 <path d="m13.7 16.29a1 1 0 1 1 -1.42 1.41l-8-8a1 1 0 0 1 0-1.41l8-8a1 1 0 1 1 1.42 1.41l-7.29 7.29z" fill-rule="evenodd"></path>
