f9629d1afeaf33122b974abf5169b04fbadd4125
Arthur Hovanesian <nameonascreen2@gmail.com>
2018-07-26 19:12:25 -0700

    Clean up syntax

--- src/components/photoview.jsx
+++ src/components/photoview.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Slider from './slider.jsx';
 import '../styles/photoview.css';
-//content, toggleCarousel
+
 class PhotoView extends React.Component {
 
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      mainPhoto: this.props.content.photos[0]
-    }
+      mainPhoto: this.props.content.photos[0],
+    };
   }
 
   componentDidMount() {
@@ -18,9 +17,9 @@ class PhotoView extends React.Component {
 
   selectPhoto(event) {
     let selected;
-    for (let i = 0; i < this.props.content.photos.length; i++) {
+    for (let i = 0; i < this.props.content.photos.length; i += 1) {
       if (event.getAttribute('src') === this.props.content.photos[i].url) {
-        selected = this.props.content.photos[i]
+        selected = this.props.content.photos[i];
       }
     }
     console.log(selected)
@@ -32,7 +31,7 @@ class PhotoView extends React.Component {
   selectNext() {
     const photos = this.props.content.photos;
     const numPhotos = this.props.content.photos.length;
-    for (let i = 0; i < numPhotos; i++) {
+    for (let i = 0; i < numPhotos; i += 1) {
       if (photos[i].url === this.state.mainPhoto.url) {
         this.setState({
           mainPhoto: photos[(i + 1) % numPhotos]
@@ -45,7 +44,7 @@ class PhotoView extends React.Component {
   selectPrevious() {
     const photos = this.props.content.photos;
     const numPhotos = this.props.content.photos.length;
-    for (let i = 0; i < numPhotos; i++) {
+    for (let i = 0; i < numPhotos; i += 1) {
       if (photos[i].url === this.state.mainPhoto.url) {
         this.setState({
           mainPhoto: photos[(((i - 1) % numPhotos) + numPhotos) % numPhotos]
@@ -69,26 +68,26 @@ class PhotoView extends React.Component {
       <div className='popup'>
         <div className='popup_inner'>
           <div>
-            <svg onClick={() => this.props.toggleCarousel()} className="exit" height="25" width="25">
+            <svg onClick={ () => this.props.toggleCarousel() } className="exit" height="25" width="25">
               <line x1="0" y1="0" x2="25" y2="25" />
               <line x1="25" y1="0" x2="0" y2="25" />
             </svg>
           </div>
           <div className="center">
-            <img onClick={this.selectNext.bind(this)} className="pic" src={this.state.mainPhoto.url}/>
+            <img onClick={ this.selectNext.bind(this) } className="pic" src={ this.state.mainPhoto.url }/>
             <div className="back" onClick={this.selectPrevious.bind(this)}>
-              <svg className="backbutton" viewBox="0 0 18 18" role="presentation" aria-hidden="true" focusable="false">
+              <svg className="backbutton" viewBox="0 0 18 18" >
                 <path d="m13.7 16.29a1 1 0 1 1 -1.42 1.41l-8-8a1 1 0 0 1 0-1.41l8-8a1 1 0 1 1 1.42 1.41l-7.29 7.29z" fill-rule="evenodd"></path>
               </svg>
             </div>
-            <div className="forward" onClick={this.selectNext.bind(this)}>
-              <svg className="forwardbutton" viewBox="0 0 18 18" role="presentation" aria-hidden="true" focusable="false">
+            <div className="forward" onClick={ this.selectNext.bind(this) }>
+              <svg className="forwardbutton" viewBox="0 0 18 18" >
                 <path d="m4.29 1.71a1 1 0 1 1 1.42-1.41l8 8a1 1 0 0 1 0 1.41l-8 8a1 1 0 1 1 -1.42-1.41l7.29-7.29z" fill-rule="evenodd"></path>
               </svg>
             </div>
           </div>
           <div className="slider">
-            <Slider content={this.props.content} mainPhoto={this.state.mainPhoto} selectPhoto={this.selectPhoto.bind(this)}/>
+            <Slider content={ this.props.content } mainPhoto={ this.state.mainPhoto } selectPhoto={ this.selectPhoto.bind(this) }/>
           </div>
         </div>
       </div>
@@ -96,4 +95,4 @@ class PhotoView extends React.Component {
   }
 }
 
-export default PhotoView
+export default PhotoView;
