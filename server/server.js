3d88ae901e4aaaa919070b01b11c78c7f98d358c
Arthur Hovanesian <nameonascreen2@gmail.com>
2018-07-29 23:03:10 -0700

    Add "Save" button functionality

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const db = require('../database/index.js');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3001;
 
 app.use('/aircnb/:id', express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/photos/:id', (req, res) => {
   const id = Number(req.params.id);
@@ -16,6 +18,19 @@ app.get('/photos/:id', (req, res) => {
     }
   });
 });
+
+app.post('/saved', (req, res) => {
+  const request = Object.keys(req.body)[0].split(', ');
+  const reqId = request[0];
+  const reqVal = request[1];
+  db.updateSave(reqId, reqVal, (err, docs) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(docs)
+    }
+  })
+})
 
 app.listen(port, () => {
   console.log('Listening on port', port);
